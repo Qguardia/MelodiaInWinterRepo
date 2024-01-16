df7e2aa0fba9f96af775936df4d1e4652c5db571
Qguardia <146794334+Qguardia@users.noreply.github.com>
2024-01-16 12:01:25 +1100

    pt1 of making bullet
    IT try to damage instead of kill

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,14 @@ public class Bullet : MonoBehaviour
 {
     public float bulletSpeed = 100.0f;
     public GameObject splatEffect;
+    
+    //Damage Scripts below
+    public bool instaDeathAttacker;
+    public float attackRate;
+    public float damageMulitplier = 1.0f;
+    private float timer;
+    public GameObject player;
+    public float health;
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +32,13 @@ public class Bullet : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         {
-            Instantiate(splatEffect, collision.transform.position, Quaternion.identity);
-            Destroy(collision.gameObject); //TEMP INSTAKILL
+           // Instantiate(splatEffect, collision.transform.position, Quaternion.identity);
+           // Destroy(collision.gameObject); //TEMP INSTAKILL
+
+            
+           DestroySelf();
         }
-        if (collision.gameObject.tag != "Player")
+        if (collision.gameObject.tag != "Enemy")
         {
             DestroySelf();
         }
@@ -37,5 +48,28 @@ public class Bullet : MonoBehaviour
     void DestroySelf()
     {
         Destroy(gameObject);
+    }
+        private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (instaDeathAttacker == true)
+            {
+                other.gameObject.GetComponent<PlayerHealth>().playerDeath();
+            }
+            else
+            {
+                health = other.gameObject.GetComponent<PlayerHealth>().playerHealth;
+
+                timer = timer + Time.deltaTime;
+
+                if (timer >= attackRate)
+                {
+                    other.gameObject.GetComponent<PlayerHealth>().playerHealth = health - 1 * damageMulitplier;
+                    timer = 0.0f;
+                }
+            }
+            
+        }
     }
 }
