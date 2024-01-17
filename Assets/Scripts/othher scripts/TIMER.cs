26c903f6b1dd1a4b17090c9d8d547c31c8766073
Qguardia <146794334+Qguardia@users.noreply.github.com>
2024-01-17 16:00:13 +1100

    QuestOBJ

---
+++ Assets/Scripts/othher scripts/TIMER.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TIMER : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+  /*  public void UpdateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+        timerText.text = string.Format("(0:00) : (1:00))", minutes, seconds);
+    }
+    public void ActivateTimer()
+    {
+
+    }
+}
+  */
