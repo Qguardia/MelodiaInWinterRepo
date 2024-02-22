c739a64483440faaef5bfe647fca46662b385d6d
Qguardia <cquitain0@gmail.com>
2024-02-22 13:38:36 +1100

    Unit selection scripts
    Unit selection scripts created, initial frame made

--- Assets/UnitSelections.cs
+++ Assets/UnitSelections.cs
@@ -1,17 +1,46 @@
+/*
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitSelections : MonoBehaviour
+// public class UnitSelections : MonoBehaviour
 {
     public List<GameObject> unitList = new List<GameObject>();
-    public List<GameObject> unitsSelect = new List<GameObject>();
+    public List<GameObject> unitsSelected = new List<GameObject>();
 
 
     private static UnitSelections _instance;
     public static UnitSelections Instance { get { return _instance; } }
 
-    private void Awake()
+   // private void Awake()
     {
-        if ()
+      //  if (_instance != null && _ != this)
+    {
+      Destroy(this.gameoObject);
     }
+     else 
+    {
+     // Make this thhe instance 
+        _instance = this; 
+    }
+*/
+/*
+public void ClickSelect(GameObject unitToAdd)
+{
+
+}
+public void ShiftclickSelect(GameObject unitToAdd)
+{
+
+}
+public void DragSelect(GameObject unitToAdd)
+{
+
+}
+public void DeselectAll()
+{
+
+}
+public void Deselect(GameObject unitToDeselect)
+{
+
 }
