1b8f801082dbc115659753de10cd66ce5ab46c38
Qguardia <cquitain0@gmail.com>
2024-02-26 15:40:22 +1100

    UNIT UPDATES

---
+++ Assets/UnitDrag.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitDrag : MonoBehaviour
+{
+    //Camera myCam
+    //[SerializeField];
+    RectTransform boxVisual;
+    // Start is called before the first frame update
+
+    Rect selectionBox;
+    Vector2 startPosition;
+    Vector2 endPosition;
+    void Start()
+    {
+        //myCam = Camera.main;
+        startPosition = Vector2.zero;
+        endPosition = Vector2.zero;
+        DrawVisual();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //on click
+       if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Input.mousePosition;
+            selectionBox = new Rect();
+        }
+        //on drag
+        if (Input.GetMouseButton(0))
+        {
+            endPosition = Input.mousePosition;
+            DrawVisual();
+            DrawSelection();
+
+        }
+        //on Drag release
+        if (Input.GetMouseButtonUp(0))
+        {
+            SelectUnits();
+            startPosition = Vector2.zero;
+            endPosition = Vector2.zero;
+            DrawVisual();
+        }
+    }
+
+    void DrawVisual()
+    {
+        Vector2 boxStart = startPosition;
+        Vector2 boxEnd = endPosition;
+
+        Vector2 boxCenter = (boxStart + boxEnd) / 2;
+        boxVisual.position = boxCenter;
+
+        Vector2 boxSize = new Vector2(Mathf.Abs(boxStart.x - boxEnd.x), Mathf.Abs(boxStart.y - boxEnd.y));
+
+        boxVisual.sizeDelta = boxSize;
+    }
+
+    void DrawSelection()
+    {
+        if(Input.mousePosition.x < startPosition.x)
+        {
+            //drag left
+            selectionBox.xMin = Input.mousePosition.x;
+            selectionBox.xMax = startPosition.x;
+        }
+        else
+        {
+            //Drag Right
+            selectionBox.xMin = startPosition.x;
+            selectionBox.xMax = Input.mousePosition.x;
+        }
+        if(Input.mousePosition.y < startPosition.y)
+        {
+            //Drag Down
+            selectionBox.yMin = Input.mousePosition.y;
+            selectionBox.yMax = startPosition.y;
+        }
+        else
+        {
+            //Drag Up
+            selectionBox.yMin = startPosition.y;
+            selectionBox.yMax = Input.mousePosition.y;
+        }
+    }
+
+    void SelectUnits()
+    {
+        foreach (var unit in UnitSelections.Instance.unitList)
+        {
+            // if unit is within bounds of selection rect
+            if (selectionBox.Contains(myCam.WorldToScreenPoint(unit.transform.position)))
+            {
+                //units within selection is added to selection
+                UnitSelections.Instance.DragSelect(unit);
+            }
+        }
+    }
+
+}
