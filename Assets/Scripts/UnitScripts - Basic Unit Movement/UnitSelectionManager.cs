89323c2bc8257b4cc36a59d4209ec747509d7cf8
Qguardia <cquitain0@gmail.com>
2024-03-12 13:17:08 +1100

    Selections

--- Assets/Scripts/UnitScripts - Basic Unit Movement/UnitSelectionManager.cs
+++ Assets/Scripts/UnitScripts - Basic Unit Movement/UnitSelectionManager.cs
@@ -9,7 +9,7 @@ public class UnitSelectionManager : MonoBehaviour
     public List<GameObject> allUnitsList = new List<GameObject>();
     public List<GameObject> UnitsSelected = new List<GameObject>();
 
-    private Camera cam;
+    public Camera cam;
     public LayerMask clickable;
     public LayerMask ground;
     public GameObject groundMarker;
@@ -35,11 +35,21 @@ public class UnitSelectionManager : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
             {
-                clickSelect(hit.collider.gameObject);
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    MultiSelect(hit.collider.gameObject);
+                }
+                else
+                {
+                    clickSelect(hit.collider.gameObject);
+                }
             }
             else
             {
-                DeselectAll();
+                if (Input.GetKey(KeyCode.LeftShift) == false)
+                {
+                    DeselectAll();
+                }
             }
         }
     }
@@ -56,6 +66,23 @@ public class UnitSelectionManager : MonoBehaviour
     }
     private void DeselectAll()
     {
-      
+        foreach (var unit in UnitsSelected)
+        {
+            EnableUnitMovement(unit, false);
+        }
+        UnitsSelected.Clear();
+    }
+    private void MultiSelect(GameObject Unit)
+    {
+        if (UnitsSelected.Contains(Unit) == false)
+        {
+            UnitsSelected.Add(Unit);
+            EnableUnitMovement(Unit, true);
+        }
+        else
+        {
+            EnableUnitMovement(Unit, false);
+            UnitsSelected.Remove(Unit);
+        }
     }
 }
