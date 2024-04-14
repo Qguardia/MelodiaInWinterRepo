036a74f2582ce56d29ffe1f89efb10f16d65ce5d
Qguardia <cquitain0@gmail.com>
2024-04-15 09:08:34 +1000

    Revert "UNIT UPDATES"
    This reverts commit 1b8f801082dbc115659753de10cd66ce5ab46c38.

--- Assets/Scripts/PlayerUnitScripts/UnitClick.cs
+++ Assets/Scripts/PlayerUnitScripts/UnitClick.cs
@@ -1,44 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitClick : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Camera myCam;
-
-    public LayerMask clickable;
-    public LayerMask ground; 
- 
     void Start()
     {
-        myCam = Camera.main;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
-            {
-                //hit clickable
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    UnitSelections.Instance.ShiftclickSelect(hit.collider.gameObject);
-
-                }
-                else
-                {
-                    UnitSelections.Instance.ClickSelect(hit.collider.gameObject);
-                }
-            }
-            else
-            {
-                //not Hit clickable
-                UnitSelections.Instance.DeselectAll();
-            }
-        }
+        
     }
 }
---
+++ Assets/UnitSelections.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelections : MonoBehaviour
+{
+    public List<GameObject> unitList = new List<GameObject>();
+    public List<GameObject> unitsSelected = new List<GameObject>();
+
+
+    private static UnitSelections _instance;
+    public static UnitSelections Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _ != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            // Make this the instance 
+            _instance = this;
+        }
+    }
+
+        public void ClickSelect(GameObject unitToAdd)
+        {
+
+        }
+        public void ShiftclickSelect(GameObject unitToAdd)
+        {
+
+        }
+        public void DragSelect(GameObject unitToAdd)
+        {
+
+        }
+        public void DeselectAll()
+        {
+
+        }
+        public void Deselect(GameObject unitToDeselect)
+        {
+
+        }
+    
+}
