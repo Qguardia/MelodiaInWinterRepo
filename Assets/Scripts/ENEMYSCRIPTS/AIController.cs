e2de554f511f487e11664a36be7d56d2581443c6
Qguardia <cquitain0@gmail.com>
2024-04-15 09:07:00 +1000

    Revert "Editing Ai"
    This reverts commit 055ab55f33ada12c60c364591658d76c37aa7085.

--- Assets/Scripts/ENEMYSCRIPTS/AIController.cs
+++ Assets/Scripts/ENEMYSCRIPTS/AIController.cs
@@ -12,8 +12,8 @@ public class AIController : MonoBehaviour
 
     public float viewRadius;
     public float viewAngle;
-    public LayerMask playerMask;
-    public LayerMask EnvironmentMask;
+    public LayerMask Clickable;
+    public LayerMask Environment;
     public float meshResolution = 1f;
     public int edgeIterations = 4;
     public float edgeDistance = 0.5f;
@@ -26,181 +26,20 @@ public class AIController : MonoBehaviour
 
     float m_WaitTime;
     float m_TimeToRotate;
- public   bool m_PlayerInRange;
- public   bool m_PlayerNear;
-  public  bool m_IsPatrol;
+    bool m_PlayerInRange;
+    bool m_PlayerNear;
+    bool m_IsPatrol;
     bool m_CaughtPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_PlayerPosition = Vector3.zero;
-        m_IsPatrol = true;
-        m_CaughtPlayer = false;
-        m_PlayerInRange = false;
-        m_WaitTime = startWaitTime;
-        m_TimeToRotate = timeToRotate;
-
-        m_CurrentWaypointIndex = 0;
-        navMeshAgent = GetComponent<NavMeshAgent>();
-
-        navMeshAgent.isStopped = false;
-        navMeshAgent.speed = speedWalk;
-        navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        EnvironmentView();
-
-        if (!m_IsPatrol)
-        {
-            Chasing();
-        }
-        else
-        {
-            Patrolling();
-        }
-    }
-
-    private void Chasing()
-    {
-        if (!m_CaughtPlayer)
-        {
-            Move(speedRun);
-            navMeshAgent.SetDestination(m_PlayerPosition);
-        }
-        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
-        {
-            if (m_WaitTime <= 0 && !m_CaughtPlayer && Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("PlayerUnit").transform.position) >= 6f)
-            {
-                m_IsPatrol = true;
-                m_PlayerNear = false;
-                Move(speedWalk);
-                m_TimeToRotate = timeToRotate;
-                m_WaitTime = startWaitTime;
-                navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-            }
-            else
-            {
-                if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("PlayerUnit").transform.position) >= 2.5f)
-                {
-                    Stop();
-                    m_WaitTime -= Time.deltaTime;
-                }
-            }
-        }
-    }
-    private void Patrolling()
-    {
-        if (m_PlayerNear)
-        {
-            if (m_TimeToRotate <= 0)
-            {
-                Move(speedWalk);
-                LookingPlayer(playerLastPosition);
-            }
-            else
-            {
-                Stop();
-                m_TimeToRotate -= Time.deltaTime;
-            }
-        }
-        else
-        {
-            m_PlayerNear = false;
-            playerLastPosition = Vector3.zero;
-            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
-            {
-                if (m_WaitTime <= 0)
-                {
-                    NextPoint();
-                    Move(speedWalk);
-                    m_WaitTime = startWaitTime;
-                }
-                else
-                {
-                    Stop();
-                    m_TimeToRotate -= Time.deltaTime;
-                }
-            }
-        }
-    }
-    void Move(float speed)
-    {
-        navMeshAgent.isStopped = false;
-        navMeshAgent.speed = speed;
-    }
-    void Stop()
-    {
-        navMeshAgent.isStopped = true;
-        navMeshAgent.speed = 0;
-    }
-    public void NextPoint()
-    {
-        m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
-        navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-    }
-    void CaughtPlayer()
-    {
-        m_CaughtPlayer = true;
-    }
-
-    void LookingPlayer(Vector3 player)
-    {
-        navMeshAgent.SetDestination(player);
-        if (Vector3.Distance(transform.position, player) <= 0.3)
-        {
-            if (m_WaitTime <= 0)
-            {
-                m_PlayerNear = false;
-                Move(speedWalk);
-                navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-                m_WaitTime = startWaitTime;
-                m_TimeToRotate = timeToRotate;
-            }
-            else
-            {
-                Stop();
-                m_WaitTime -= Time.deltaTime;
-            }
-        }
-    }
-    void EnvironmentView()
-    {
-        Collider[] playerInRange = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
-
-        for (int i = 0; i < playerInRange.Length; i++)
-        {
-            Transform player = playerInRange[i].transform;
-            Vector3 dirToPlayer = (player.position - transform.position).normalized;
-            if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
-            {
-                float dstToPlayer = Vector3.Distance(transform.position, player.position);
-                if (!Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, EnvironmentMask))
-                {
-                    m_PlayerInRange = true;
-                    m_IsPatrol = false;
-                }
-                else
-                {
-                    m_PlayerInRange = false;
-                    Debug.Log("Returning To patrol");
-                    m_IsPatrol = true;
-                }
-            }
-            if (Vector3.Distance(transform.position, player.position) > viewRadius)
-            {
-                m_PlayerInRange = false;
-            }
-
-            if (m_PlayerInRange)
-            {
-                m_PlayerPosition = player.transform.position;
-            }
-
-        }
+        
     }
 }
--- Assets/Scripts/UnitScripts - Basic Unit Movement/UnitMovement.cs
+++ Assets/Scripts/UnitScripts - Basic Unit Movement/UnitMovement.cs
@@ -9,9 +9,6 @@ public class UnitMovement : MonoBehaviour
     NavMeshAgent agent;
     public LayerMask ground;
 
-    //Below are InputScripts for different abilties
-    
-
     private void Start()
     {
         cam = Camera.main;
